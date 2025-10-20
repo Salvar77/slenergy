56a662ebb29121f44aa644f1955c76b15e5221cf
Łukasz <lukaszkus77@gmail.com>
2025-10-20 11:34:18 +0200

    Deploy to production

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Footer from "@/components/Footer/Footer";
 import "/styles/globals.scss";
 import Nav from "@/components/Nav/Nav";
-import { DefaultSeo } from "next-seo";
 
 export default function App({ Component, pageProps }) {
   return (
